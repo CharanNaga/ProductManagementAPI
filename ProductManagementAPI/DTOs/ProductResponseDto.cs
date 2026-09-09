d7f42afaa23b6164e9c5364401d166930fcf1d29
charan <nagasaicharansinghbondili@gmail.com>
2026-09-09 06:36:34 +0530

    Defined DTO Classes

---
+++ ProductManagementAPI/DTOs/ProductResponseDto.cs
@@ -0,0 +1,14 @@
+﻿namespace ProductManagementAPI.DTOs
+{
+    public class ProductResponseDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string? Description { get; set; }
+        public decimal Price { get; set; }
+        public int StockQuantity { get; set; }
+        public string Category { get; set; } = null!;
+        public bool IsActive { get; set; }
+        public DateTime CreatedOn { get; set; }
+    }
+}
