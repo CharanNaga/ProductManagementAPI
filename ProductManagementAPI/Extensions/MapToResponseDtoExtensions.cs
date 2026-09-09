b9c85982aaa6b70610705f69c21a0f480f81cc5f
charan <nagasaicharansinghbondili@gmail.com>
2026-09-09 07:07:22 +0530

    Defined Service Classes with Extensions helper

---
+++ ProductManagementAPI/Extensions/MapToResponseDtoExtensions.cs
@@ -0,0 +1,24 @@
+﻿using ProductManagementAPI.DTOs;
+using ProductManagementAPI.Models;
+
+namespace ProductManagementAPI.Extensions
+{
+    public static class MapToResponseDtoExtensions
+    {
+        // Converts Product entity into ProductResponseDto
+        public static ProductResponseDto MapToResponseDto(this Product product)
+        {
+            return new ProductResponseDto
+            {
+                Id = product.Id,
+                Name = product.Name,
+                Description = product.Description,
+                Price = product.Price,
+                StockQuantity = product.StockQuantity,
+                Category = product.Category,
+                IsActive = product.IsActive,
+                CreatedOn = product.CreatedOn
+            };
+        }
+    }
+}   
---
+++ ProductManagementAPI/Services/IProductService.cs
@@ -0,0 +1,23 @@
+﻿using ProductManagementAPI.DTOs;
+
+namespace ProductManagementAPI.Services
+{
+    public interface IProductService
+    {
+
+        // Returns all products as response DTOs
+        Task<List<ProductResponseDto>> GetAllProductsAsync();
+
+        // Returns a single product as response DTO based on the given Id
+        Task<ProductResponseDto?> GetProductByIdAsync(Guid id);
+
+        // Creates a new product using the incoming create DTO
+        Task<ProductResponseDto> CreateProductAsync(ProductCreateDto dto);
+
+        // Updates an existing product based on the update DTO
+        Task<bool> UpdateProductAsync(ProductUpdateDto dto);
+
+        // Deletes an existing product based on the given Id
+        Task<bool> DeleteProductAsync(Guid id);
+    }
+}
